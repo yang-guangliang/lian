751c062449a0717cfacfcd1e39e7cc337c992e65
Yang Guangliang <yanggl@fudan.edu.cn>
2025-04-20 22:24:22 +0800

    update lang_config

--- src/lian/config/lang_config.py
+++ src/lian/config/lang_config.py
@@ -2,24 +2,17 @@ from dataclasses import dataclass
 import os
 from lian.lang import (
     c_parser,
-    cpp_parser,
     csharp_parser,
-    rust_parser,
     go_parser,
     java_parser,
     javascript_parser,
     python_parser,
     ruby_parser,
-    swift_parser,
-    kotlin_parser,
-    scala_parser,
     php_parser,
-    safe_parser
+    llvm_parser
 )
 LIB_DIR              = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 LANGS_SO_PATH        = os.path.join(LIB_DIR, "lib/langs_linux.so")
-ABC_LANG_SO_PATH     = os.path.join(LIB_DIR, "lib/abc_lang_linux.so")
-SAFE_LANG_SO_PATH    = os.path.join(LIB_DIR, "lib/safe_lang_linux.so")
 
 @dataclass
 class LangConfig:
@@ -33,18 +26,13 @@ LANG_TABLE = [
     LangConfig(name = "c", extension = [".c", ".h", ".i",], parser = c_parser.Parser),
     LangConfig(name = "cpp", extension = [".cpp", ".cxx", ".cc", ".h", ".hh", ".hpp", ".ii"], parser = cpp_parser.Parser),
     LangConfig(name = "csharp", extension = [".cs"], parser = csharp_parser.Parser),
-    LangConfig(name = "rust", extension = [".rs"],parser = rust_parser.Parser),
     LangConfig(name = "go", extension = [".go"], parser = go_parser.Parser),
     LangConfig(name = "java", extension = [".java"], parser = java_parser.Parser),
     LangConfig(name = "javascript", extension = [".js"], parser = javascript_parser.Parser),
     LangConfig(name = "python", extension = [".py"], parser = python_parser.Parser),
-    LangConfig(name = "ruby", extension = [".rb"], parser = rust_parser.Parser),
-    LangConfig(name = "swift", extension = [".swift"], parser = swift_parser.Parser),
-    LangConfig(name = "kotlin", extension = [".kt"], parser = kotlin_parser.Parser),
-    LangConfig(name = "scala", extension = [".scala"], parser = scala_parser.Parser),
     LangConfig(name = "php", extension = [".php"], parser = php_parser.Parser),
     LangConfig(name = "ruby", extension = [".rb"], parser = ruby_parser.Parser),
-    LangConfig(name = "safe", extension = [".safe"], parser = safe_parser.Parser),
+    LangConfig(name = "llvm", extension = [".ll"], parser = llvm_parser.Parser),
 ]
 
 LANG_EXTENSIONS = {}
