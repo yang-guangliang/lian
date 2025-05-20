0eb47925c0727ac058c1ac6ab7ac20977cf3e6ef
Rainy <ruininggao@126.com>
2025-05-20 17:03:47 +0800

    update lian.sh

--- scripts/lian.sh
+++ scripts/lian.sh
@@ -1,20 +1,34 @@
 #! /bin/sh
 
+clear
+
+if [ $# -eq 0 ]; then
+    echo "$0 <path>"
+    exit 1
+fi
+
+
 ROOT_DIR=$(realpath $0)
 ROOT_DIR=$(dirname $ROOT_DIR)
 ROOT_DIR=$(dirname $ROOT_DIR)
 
 # TARGET_PATH=$(realpath "$1")
 
-#CMD="python -W ignore"s
+#CMD="python -W ignore"
 CMD="python $ROOT_DIR/src/lian/interfaces/main.py"
-OPTIONS="run"
+OUTPUT_PATH="$ROOT_DIR/tests/lian_workspace"
+# OPTIONS="run -f -p -d -l python,java,c -w $OUTPUT_PATH"
+OPTIONS="run -f -d -l python,php,javascript,java -w $OUTPUT_PATH"
 
-if [ $# -eq 0 ]; then
-    $CMD $OPTIONS --help
-    exit 1
-fi
-
+# echo $CMD $OPTIONS $TARGET_PATH
+# $CMD $OPTIONS $TARGET_PATH
 $CMD $OPTIONS $@
 
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
+
+echo "============= Output dataframe files =============="
+
+python $ROOT_DIR/scripts/dfview.py $OUTPUT_PATH
 
