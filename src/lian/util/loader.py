8f06beab249d9aed0f16097c343ed6c0a7b96433
Rainy <ruininggao@126.com>
2025-09-23 23:21:28 +0800

    新增：loader方法load_method_in_class_with_method_name, 从给定类中根据方法名找到对应类中方法

--- src/lian/util/loader.py
+++ src/lian/util/loader.py
@@ -2612,6 +2612,8 @@ class Loader:
                 return method_id
         return -1
 
+
+
     def load_root_path(self):
         """返回扫描的根路径"""
         return getattr(self.options, "in_path", "")
