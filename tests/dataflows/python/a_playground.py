6b97f46a49dfac36f0cc0fccc6a424fe000b580f
梅傲寒 <3472274613@qq.com>
2025-12-17 15:42:06 +0800

    fix recover call name

--- tests/dataflows/python/a_playground.py
+++ tests/dataflows/python/a_playground.py
@@ -4,6 +4,10 @@
 #     m = prop(c)
 #     sink(a)
 # func1()
+import pickle
+
 latents_ubyte = (
     a.mul(0xFF)  # change scale from -1..1 to 0..1  # to 0..255
 ).to(device="cpu", dtype=torch.uint8)
+
+pickle.load(abc)
