12b13e01f6284d18ca8bd03cd1b6c38a537c0dac
梅傲寒 <3472274613@qq.com>
2025-12-18 19:14:39 +0800

    add python so

--- tests/dataflows/python/a_playground.py
+++ tests/dataflows/python/a_playground.py
@@ -1,11 +1,14 @@
-# def func1():
+# p = 3
+# def func1(q):
+#     gg = q
 #     a = source()
 #     c = a.b.h
 #     m = prop(c)
 #     sink(a)
-# func1()
+# func1(p)
 # latents_ubyte = (
 #     a.mul(0xFF)  # change scale from -1..1 to 0..1  # to 0..255
 # ).to(device="cpu", dtype=torch.uint8)
-a = s()
-b + c
+# a = s()
+# b + c
+
