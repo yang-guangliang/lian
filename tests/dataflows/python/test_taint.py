acfafbcae9271c97fd379ec732d0fe813cc7a657
梅傲寒 <3472274613@qq.com>
2025-12-23 08:07:44 +0000

    !98 fix field_read source * fix field_read source

--- tests/dataflows/python/test_taint.py
+++ tests/dataflows/python/test_taint.py
@@ -1,13 +1,18 @@
-func0()
-def a():
-    t = source()
+# func0()
+# def a():
+#     t = source()
+#
+#     b(t)
+# def b(p):
+#     g = p
+#     sink(g)
+# def c(p, p1):
+#     sink(p)
+#     send(p1)
+# def func0():
+#     a()
 
-    b(t)
-def b(p):
-    g = p
-    sink(g)
-def c(p, p1):
-    sink(p)
-    send(p1)
-def func0():
-    a()
+a = b.c
+
+gg = a
+sink(gg)
