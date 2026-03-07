f09147e2ae1ff696769f18a779801cd9dc9fc48b
梅傲寒 <3472274613@qq.com>
2026-03-07 16:07:35 +0800

    add try test code

--- tests/control_flows/try.py
+++ tests/control_flows/try.py
@@ -12,3 +12,15 @@ def cfg_try():
         print("Result:", result)
     finally:
         print("This block is executed no matter what.")
+
+def run(code, globals_env=None, locals_env=None):
+    try:
+        tmp_code = ""
+        for line in code.split("\n"):
+            if not line.startswith("```"):
+                tmp_code += line + "\n"
+
+        exec(tmp_code, globals_env, locals_env)
+    except Exception as e:
+        return str(e)
+    return None
